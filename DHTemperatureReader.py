fdc1f619422cebe81bb3b57fa559728a2476848f
Alan Mangroo <alan.mangroo@gmail.com>
2018-11-24 11:46:08 +1100

    DHT11 sensor

---
+++ DHTemperatureReader.py
@@ -0,0 +1,35 @@
+import os
+
+import time
+import urllib
+
+import sys
+import Adafruit_DHT
+
+def dhTemp():
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    return temperature
+
+def post_data(temp):
+    try:
+        url = "https://api.thingspeak.com/update?api_key=LJ4VRHELTZKXIIXK&field1="+temp
+        response = urllib.urlopen(url).read()
+    except IOError:
+        print "ERROR WHILE POSTING DATA!"
+    return response
+
+while True:
+    try:
+        t = str(dhTemp())
+        resp = post_data(t)
+        print(resp)
+        print(t)
+        f= open('temp.csv','a')
+        f.write(time.strftime("%H:%M:%S") + ',' )
+        f.write(t)
+        f.write('\n')
+        f.close()
+        time.sleep(60)
+    except Exception:
+        print "ERROR IN WHILE!"
