f55a4f864734c29ce8e43e136c48f8f9981902e7
alan.mangroo@gmail.com <alan.mangroo@gmail.com>
2018-11-24 12:10:59 +1100

    DHT11 humidity sensor

--- DHTemperatureReader.py
+++ DHTemperatureReader.py
@@ -12,7 +12,9 @@ hu = 0
 def dhTemp():
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    global tm 
     tm = temperature
+    global hu
     hu = humidity
     return temperature
 
