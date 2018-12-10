620aeffd345caea353f1902d6bdeac183c847720
alan.mangroo@gmail.com <alan.mangroo@elabor8.com.au>
2018-12-10 20:17:45 +1100

    switch to dh22

--- DHTemperatureReader.py
+++ DHTemperatureReader.py
@@ -10,7 +10,7 @@ tm = 0
 hu = 0
 
 def dhTemp():
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
     print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
     global tm 
     tm = temperature
