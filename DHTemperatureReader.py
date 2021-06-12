b2ffdbfb0868720649b81e3062fde089d27d2779
Alan Mangroo <alan.mangroo@gmail.com>
2021-06-12 18:17:11 +1000

    influxdb

--- DHTemperatureReader.py
+++ DHTemperatureReader.py
@@ -19,7 +19,7 @@ def dhTemp():
     humidity, temperature = Adafruit_DHT.read_retry(22, 4)
     print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
     global tm 
-    tm = temperature
+    tm = '{0:0.1f}'.format(temperature)
     global hu
     hu = humidity
     return temperature
