b7a86ee30136d4904e1f4ee75ed556116cfaa09c
Alan Mangroo <alan.mangroo@gmail.com>
2021-06-12 16:41:45 +1000

    python3

--- DHTemperatureReader.py
+++ DHTemperatureReader.py
@@ -9,7 +9,7 @@ hu = 0
 
 def dhTemp():
     humidity, temperature = Adafruit_DHT.read_retry(22, 4)
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
     global tm 
     tm = temperature
     global hu
